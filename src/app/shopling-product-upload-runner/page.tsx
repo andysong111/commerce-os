9980e754c5334756bbb3636e10dba9b41a53384c
andysong111 <andy0801a@gmail.com>
2026-07-28 06:13:26 +0900

    Document card 14 location code barcode mirror

--- src/app/shopling-product-upload-runner/page.tsx
+++ src/app/shopling-product-upload-runner/page.tsx
@@ -6,7 +6,7 @@ export default function ShoplingProductUploadRunnerPage() {
     <>
       <PageHeader
         title="샵플링 상품등록 실행기"
-        description="실재고 시트 행 번호를 입력해 shopling-product-upload-auto 외부 엔진을 실행합니다."
+        description="실재고 시트 행 번호를 입력해 shopling-product-upload-auto 외부 엔진을 실행하며 위치코드를 옵션자체관리코드와 바코드에 동일하게 등록합니다."
       />
       <ShoplingProductUploadRunner />
     </>
