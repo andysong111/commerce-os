8158bd9b709aeeb560287e99c4911e8e14b4da67
andysong111 <andy0801a@gmail.com>
2026-08-02 02:27:02 +0900

    Load category update progress dialog

--- public/product-launch-tracker-app/app.js
+++ public/product-launch-tracker-app/app.js
@@ -7,3 +7,4 @@ await import("./china-product-links.js");
 await import("./table-inline-ops-loader.js");
 await import("./category-ai.js");
 await import("./category-toolbar-layout.js");
+await import("./category-update-progress.js");
