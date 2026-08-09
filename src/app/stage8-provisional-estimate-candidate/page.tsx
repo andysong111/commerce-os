0476e1015f5dd6f1179d12d95c05473835c91797
andysong111 <andy0801a@gmail.com>
2026-08-10 04:24:55 +0900

    Add provisional estimate candidate page

---
+++ src/app/stage8-provisional-estimate-candidate/page.tsx
@@ -0,0 +1,7 @@
+import { PageHeader } from "@/components/PageHeader";
+import { loadProvisionalEstimateCandidate } from "@/lib/stage8ProvisionalEstimateCandidate";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+const number = new Intl.NumberFormat("ko-KR"Aborted
