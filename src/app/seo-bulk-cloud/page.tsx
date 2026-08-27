53cf9e6d7b4151327fcbf0c7350e30e2058a9dec
andysong111 <andy0801a@gmail.com>
2026-08-27 21:06:32 +0900

    feat: switch SEO cloud to run-instance client

--- src/app/seo-bulk-cloud/page.tsx
+++ src/app/seo-bulk-cloud/page.tsx
@@ -1,23 +1,15 @@
-import SeoBulkCloudClient from "./SeoBulkCloudClient";
-import SeoBulkCompletionArchiveBridge from "./SeoBulkCompletionArchiveBridge";
-import SeoBulkExistingFinalDiversityBridge from "./SeoBulkExistingFinalDiversityBridge";
 import SeoBulkFetchRecovery from "./SeoBulkFetchRecovery";
-import SeoBulkInventoryReadyReregister from "./SeoBulkInventoryReadyReregister";
 import SeoBulkMallSeoRecoveryBridge from "./SeoBulkMallSeoRecoveryBridge";
+import SeoBulkRunCloudClient from "./SeoBulkRunCloudClient";
 import SeoBulkWindowBridge from "./SeoBulkWindowBridge";
 
-// SeoBulkInventoryReregisterBridge is rendered inside SeoBulkInventoryReadyReregister
-// only after the FINAL-keyword v4 inventory sync succeeds.
 export default function SeoBulkCloudPage() {
   return (
     <>
       <SeoBulkWindowBridge />
       <SeoBulkFetchRecovery />
       <SeoBulkMallSeoRecoveryBridge />
-      <SeoBulkCompletionArchiveBridge />
-      <SeoBulkExistingFinalDiversityBridge />
-      <SeoBulkInventoryReadyReregister />
-      <SeoBulkCloudClient />
+      <SeoBulkRunCloudClient />
     </>
   );
 }
