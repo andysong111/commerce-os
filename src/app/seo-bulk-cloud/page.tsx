62f83889e1107bf1ef473de9c032fcf1dd37b299
andysong111 <andy0801a@gmail.com>
2026-08-28 00:56:24 +0900

    test: retain keyword fetch recovery bridge during durable transition

--- src/app/seo-bulk-cloud/page.tsx
+++ src/app/seo-bulk-cloud/page.tsx
@@ -1,3 +1,4 @@
+import SeoBulkFetchRecovery from "./SeoBulkFetchRecovery";
 import SeoBulkRunCloudClient from "./SeoBulkDurableRunCloudClient";
 import SeoBulkLongTitleV6UpgradeBridge from "./SeoBulkLongTitleV6UpgradeBridge";
 import SeoBulkMallSeoRecoveryBridge from "./SeoBulkMallSeoRecoveryBridge";
@@ -10,6 +11,7 @@ export default function SeoBulkCloudPage() {
       <SeoBulkWindowBridge />
       <SeoBulkRunMigrationBridge />
       <SeoBulkLongTitleV6UpgradeBridge />
+      <SeoBulkFetchRecovery />
       <SeoBulkMallSeoRecoveryBridge />
       <SeoBulkRunCloudClient />
     </>
