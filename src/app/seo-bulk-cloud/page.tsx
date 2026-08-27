3202eac31c388c30789ec39daf9f70fef786a611
andysong111 <andy0801a@gmail.com>
2026-08-28 00:49:01 +0900

    feat: switch SEO cloud to durable server run client

--- src/app/seo-bulk-cloud/page.tsx
+++ src/app/seo-bulk-cloud/page.tsx
@@ -1,7 +1,6 @@
-import SeoBulkFetchRecovery from "./SeoBulkFetchRecovery";
+import SeoBulkDurableRunCloudClient from "./SeoBulkDurableRunCloudClient";
 import SeoBulkLongTitleV6UpgradeBridge from "./SeoBulkLongTitleV6UpgradeBridge";
 import SeoBulkMallSeoRecoveryBridge from "./SeoBulkMallSeoRecoveryBridge";
-import SeoBulkRunCloudClient from "./SeoBulkRunCloudClient";
 import SeoBulkRunMigrationBridge from "./SeoBulkRunMigrationBridge";
 import SeoBulkWindowBridge from "./SeoBulkWindowBridge";
 
@@ -11,9 +10,8 @@ export default function SeoBulkCloudPage() {
       <SeoBulkWindowBridge />
       <SeoBulkRunMigrationBridge />
       <SeoBulkLongTitleV6UpgradeBridge />
-      <SeoBulkFetchRecovery />
       <SeoBulkMallSeoRecoveryBridge />
-      <SeoBulkRunCloudClient />
+      <SeoBulkDurableRunCloudClient />
     </>
   );
 }
