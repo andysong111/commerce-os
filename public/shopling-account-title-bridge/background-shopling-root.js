49999772caa43aee587a61e27a4746752f820f76
andysong111 <andy0801a@gmail.com>
2026-08-30 21:27:20 +0900

    Load OPS registry title worker

--- public/shopling-account-title-bridge/background-shopling-root.js
+++ public/shopling-account-title-bridge/background-shopling-root.js
@@ -2,6 +2,7 @@
 
 importScripts(
   "background-shopling-title-batch.js",
+  "background-shopling-title-registry.js",
   "background-shopling-seo-keywords.js",
   "background-shopling-pipeline.js",
 );
